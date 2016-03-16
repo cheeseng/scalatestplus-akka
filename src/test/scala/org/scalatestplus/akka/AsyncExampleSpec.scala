029747e0c1ee5b4ef429c82cb6b115fee300749c
Leire Fernandez de Retana <leire.retana@workday.com>
2016-03-16 12:31:42 +0000

    ReceivingN implementation plus one positive/negative test per method

--- src/test/scala/org/scalatestplus/akka/AsyncExampleSpec.scala
+++ src/test/scala/org/scalatestplus/akka/AsyncExampleSpec.scala
@@ -15,21 +15,18 @@
  */
 package org.scalatestplus.akka
 
+import java.util.concurrent.ExecutionException
+
 import akka.actor.ActorSystem
-import akka.actor.Actor
-import akka.actor.Props
-import akka.testkit.{ TestActors, TestKit, ImplicitSender }
-import org.scalatest.BeforeAndAfterAll
-import org.scalatest.Matchers
-import org.scalatest.WordSpecLike
+import akka.testkit.{TestActors, TestKit, ImplicitSender}
 import org.scalatest.Matchers
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.AsyncWordSpecLike
+import org.scalatest.time.{Seconds, Span}
 
-import scala.concurrent.Future
 
 class AsyncExampleSpec(system: ActorSystem) extends TestKit(system) with AsyncTestKitLike with ImplicitSender
-  with AsyncWordSpecLike with Matchers with BeforeAndAfterAll {
+with AsyncWordSpecLike with Matchers with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("ExampleSpec"))
 
@@ -37,12 +34,48 @@ class AsyncExampleSpec(system: ActorSystem) extends TestKit(system) with AsyncTe
     TestKit.shutdownActorSystem(system)
   }
 
-  "An Echo actor" should {
-    "send back messages unchanged" in {
+  "An Echo actor with implicit Patience" should {
+    "send back 3 messages unchanged" in {
       val echo = system.actorOf(TestActors.echoActorProps)
       echo ! "hello world"
-      val fut = Future { expectMsg("hello world") }
+      echo ! "hello world2"
+      echo ! "hello world3"
+      val fut = receivingN(3)
       fut.map(_ => succeed)
+    }
+
+    "fail if it expects 2 messages but receives 1" in {
+      val echo = system.actorOf(TestActors.echoActorProps)
+      echo ! "hello world"
+
+      recoverToExceptionIf[ExecutionException] {
+        receivingN(2)
+      } map { e =>
+        e.getCause().getMessage shouldBe "assertion failed: timeout (3 seconds) while expecting 2 messages (got 1)"
+      }
+    }
+  }
+
+  "An Echo actor with explicit Span" should {
+    "send back 3 messages unchanged" in {
+      val echo = system.actorOf(TestActors.echoActorProps)
+      echo ! "hello world"
+      echo ! "hello world2"
+      echo ! "hello world3"
+      val fut = receivingN(3, Span(1, Seconds))
+      fut.map(_ => succeed)
+    }
+  }
+
+  "fail if it expects 2 messages but receives 1" in {
+    val timeout = 2
+    val echo = system.actorOf(TestActors.echoActorProps)
+    echo ! "hello world"
+
+    recoverToExceptionIf[ExecutionException] {
+      receivingN(2, Span(timeout, Seconds))
+    } map { e =>
+      e.getCause().getMessage shouldBe s"assertion failed: timeout ($timeout seconds) while expecting 2 messages (got 1)"
     }
   }
 }
